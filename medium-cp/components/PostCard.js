0e121ce776a6239ee938fa403130ca631c7c6b2e
Chuxuan Quan <qcx60990@gmail.com>
2022-09-12 23:28:47 -0400

    commit

---
+++ medium-cp/components/PostCard.js
@@ -0,0 +1,59 @@
+import Logo from "../static/logo.png";
+import Image from "next/image";
+import { FiBookmark } from "react-icons/fi";
+
+const styles = {
+  authorContainer: "flex gap-[.4rem]",
+  authorImageContainer:
+    "grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]",
+  postDetails: "flex-[2.5] flex flex-col",
+  detailsContainer: "flex items-center justify-between text-[#787878]",
+  articleDetails: "my-2 text-[.8rem]",
+  authorImage: "object-cover",
+  authorName: "font-semibold",
+  title: "font-bold text-2xl",
+  briefing: "text-[#787878]",
+  category: "bg-[#F2F3F2] p-1 rounded-full",
+  bookmarkContianer: "cursor-pointer",
+  wrapper:
+    "flex max-w-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer",
+  thumbnailContainer: "",
+};
+
+const PostCard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.postDetails}>
+        <div className={styles.authorContainer}>
+          <div className={styles.authorImageContainer}>
+            <Image
+              src={Logo}
+              className={styles.authorImage}
+              width={40}
+              height={40}
+            />
+          </div>
+          <div className={styles.authorName}>Chuxuan Quan</div>
+        </div>
+        <h1 className={styles.title}>
+          7 Free Tools That Will Make Your More Productive
+        </h1>
+        <div className={styles.briefing}>Productivity that can be learned</div>
+        <div className={styles.detailsContainer}>
+          <span className={styles.articleDetails}>
+            Jun 15 • 5 min read •{" "}
+            <span className={styles.category}>productivity</span>
+          </span>
+          <span className={styles.bookmarkContianer}>
+            <FiBookmark className="h-5 w-5"></FiBookmark>
+          </span>
+        </div>
+      </div>
+      <div className={styles.thumbnailContainer}>
+        <Image src={Logo} height={100} width={100}></Image>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- medium-cp/pages/index.js
+++ medium-cp/pages/index.js
@@ -1,3 +1,31 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import PostCard from "../components/PostCard";
+
+const styles = {
+  postList:
+    "flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3",
+  container: "max-w-7xl flex-1",
+  main: "flex justify-center",
+};
+
 export default function Home() {
-  return <h1>Wooho</h1>;
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.postList}>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
